5897864e1f330758175fe0341f8ed78f9b4aa159
Petar Partaloski <ppartaloski@yahoo.com>
2022-02-23 18:37:48 +0100

    Second commit, fixed bug.

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php 
 	include_once("index.html");
-	include_once("script.js");
-	include_once("style.css");
 	include_once("img/AlphaTauri.png");
 	include_once("img/AstonMartin.png");
 	include_once("img/Alpine.png");
